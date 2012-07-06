2ea72afb6df83b697ff588c7c8c9a466c99cf85f
3rd-Eden <info@3rd-Eden.com>
2012-07-06 14:10:04 +0200

    Retry operations

--- notify.js
+++ notify.js
@@ -2,6 +2,7 @@
 
 var EventEmitter = require('events').EventEmitter
   , async = require('async')
+  , retry = require('retry')
   , fs = require('fs');
 
 /**
@@ -14,9 +15,9 @@ var EventEmitter = require('events').EventEmitter
 function Notify (files) {
   this.FSWatchers = {};       // stores the watchers
   this.FStats = {};           // latest file stats
-  this.retries = {};          // retries
+  this.retry = {};            // files we are retrying
 
-  this.maxRetries = 5;        // amount of retries we can do per file
+  this.maxRetries = 10;       // amount of retries we can do per file
 
   if (files) this.add(files);
 }
@@ -39,8 +40,8 @@ Notify.prototype.add = function add(files) {
   if (!Array.isArray(files)) files = [files];
 
   // filter out any non existing files
-  async.filter(files, fs.exist, function (files) {
-    files.forEach(self.watch);
+  async.filter(files, fs.exists, function (files) {
+    files.forEach(self.watch, self);
   });
 
   return this;
@@ -115,17 +116,25 @@ Notify.prototype.manually = function manually(FSWatcher) {
   // loop over the files and compare the fs.Stat's
   files.forEach(function test(file) {
     var current = self.FStats[file];
-    if (!current) return; // unknown file
+    if (!current) return;
 
-    fs.stat(function stats(err, stat) {
-      if (!stat || err) return;
+    // make sure that the file we are going to check actually exists.. or we
+    // will get a failed fs.stat operation
+    self.ensure(file, function existance(exists) {
+      if (!exists) return;
 
-      // check if the modification time has changed
-      if (current.mtime !== stat.mtime) {
-        self.emit('change', file, stat);
-      }
+      fs.stat(file, function stats(err, stat) {
+        if (!stat || err) return;
+
+        // check if the modification time has changed
+        if (current.mtime !== stat.mtime) {
+          self.emit('change', file, stat);
+        }
+      });
     });
   });
+
+  return this;
 };
 
 /**
@@ -135,16 +144,69 @@ Notify.prototype.manually = function manually(FSWatcher) {
  */
 
 Notify.prototype.reset = function reset(path) {
-  var FSWatcher = this.FSWatcher[path];
-  FSWatcher.close();
+  var self = this
+    , FSWatcher = this.FSWatchers[path];
+
+  // close it
+  if (FSWatcher) FSWatcher.close();
 
   // clear it from our queue
-  delete this.FSWatcher[path];
+  delete this.FSWatchers[path];
   delete this.FStats[path];
 
-  this.watch(path);
+  // check if we already have a retry operation running, as multiple events can
+  // trigger this call..
+  if (this.retry[path]) return this;
+
+  this.retry[path] = true;
+  return this.ensure(path, function existance(exists) {
+    delete self.retry[path];
+
+    if (!exists) {
+      return self.emit('error', new Error('File ' + path + ' is gone'), path);
+    }
+
+    self.watch(path);
+  });
 };
 
+/**
+ * Small helper function to ensure that a file available.. As IDE's could be
+ * writing the new result to a new file, remove the current file and put the new
+ * file in to place. So during that operation it could be that we do a fs.stat
+ * and get a missing file.
+ *
+ * It could still happen here.. but it's less likely that the file is removed
+ * after we have detected it's existance again.
+ *
+ * @param {String} path
+ * @param {Function} fn
+ * @api private
+ */
+
+Notify.prototype.ensure = function ensure(path, fn) {
+  var operation = retry.operation({
+      retries: this.maxRetries
+    , factor: 3
+    , minTimeout: 100
+    , maxTimeout: 60 * 100
+    , randomize: true
+  });
+
+  // do some fault tolerant existance checking
+  operation.attempt(function attempt() {
+    fs.exists(path, function existance(exists) {
+      // we are using .exists, and that doens't return an error just a boolean,
+      // so we need to create a fake error object for your retry operation
+      var fakeerr = exists ? undefined : new Error();
+      if (operation.retry(fakeerr)) return;
+
+      fn(exists);
+    });
+  });
+
+  return this;
+};
 
 /**
  * A file change has been triggered.
@@ -156,22 +218,12 @@ Notify.prototype.reset = function reset(path) {
  */
 
 Notify.prototype.change = function change(FSWatcher, event, filename) {
-  if (!filename) this.manually(FSWatcher);
+  if (!filename || true) return this.manually(FSWatcher).reset(FSWatcher.path);
 
-  var self = this
-    , path = FSWatcher.path;
+  this.emit('change', filename);
+  this.reset(FSWatcher.path);
 
-  // get the latest fs.stat object
-  fs.stat(fs, function stats(err, stat) {
-    if (stat) self.FStats[path] = stat;
-    if (err) return;
-
-    // we need to check if we had a file that was changed, it could be that we
-    // are watching a directory and that a file inside that directory has
-    // changed
-    var file = stat.isDirectory() ? filename : path;
-    self.emit('change', file, stat);
-  });
+  return this;
 };
 
 /**
@@ -183,15 +235,7 @@ Notify.prototype.change = function change(FSWatcher, event, filename) {
  */
 
 Notify.prototype.error = function error(FSWatcher, err) {
-  // @TODO retry watching again
-  var path = FSWatcher.path;
-
-  // clear it from our queue
-  delete this.FSWatcher[path];
-  delete this.FStats[path];
-
-  // emit removed
-  this.emit('removed', path);
+  return this.reset(FSWatcher.path);
 };
 
 // expose the notifier
@@ -218,4 +262,4 @@ Notify.watch = function watch(file, callback) {
  * @api private
  */
 
-Notify.version = require('package.json').version;
+Notify.version = require('./package.json').version;
